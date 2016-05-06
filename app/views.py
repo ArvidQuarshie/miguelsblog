d2bb8773b82a4cda7f12ab32673e0e56bb638ebc
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-06 18:53:17 +0300

    edits

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'nickname': 'Miguel'}  # fake user
+    user = {'nickname': 'Arvid'}  # fake user
     return render_template('index.html',
                            title='Home',
                            user=user)
