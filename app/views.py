81dd267dee080e046feffcfbf668adf953433a98
ArvidQuarshie <arvidquarshie@gmail.com>
2016-05-09 12:42:10 +0300

    views.py

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 #returns string to be displayed on the client's browser
-from flask import render_template
-from app import app
-
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'Arvid'}  # fake user
-    return render_template('index.html',
-                           title='Home',
-                           user=user)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', 
+                           title='Sign In',
+                           form=form)
